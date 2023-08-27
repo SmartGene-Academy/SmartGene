e0961fd753ff9bf2472be7f41b3c5155efe10fd9
Saai151 <94809929+Saai151@users.noreply.github.com>
2023-08-27 16:45:44 -0400

    Sign Up page using firebase, has an error

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,46 +1,65 @@
-import React, { useRef } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from '../Components/navbar';
-import { Container, Form, Button, Card } from 'react-bootstrap';
+import React, { useRef, useState } from "react"
+import { Form, Button, Card, Alert } from "react-bootstrap"
+import { useAuth } from "../contexts/AuthContext"
+import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
-const Login = () => {
-  const emailRef = useRef();
-  const passwordRef = useRef(); // Create a useRef reference for the password input
-  const passwordConfirmRef = useRef(); // Create a useRef reference for the password confirmation input
+export default function Signup() {
+  const emailRef = useRef()
+  const passwordRef = useRef()
+  const passwordConfirmRef = useRef()
+  const { signup } = useAuth()
+  const [error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
+  const history = useNavigate()
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError("Passwords do not match")
+    }
+
+    try {
+      setError("")
+      setLoading(true)
+      await signup(emailRef.current.value, passwordRef.current.value)
+      history.push("/")
+    } catch {
+      setError("Failed to create an account")
+    }
+
+    setLoading(false)
+  }
 
   return (
-    <div>
-      <NavBar></NavBar>
-      <Container className='d-flex allign-items-center justify-content-center' style={{minHeight: "100vh"}}>
-      <div className='w-100' style={{maxWidth: "400px"}}>
-          <Card>
-            <Card.Body>
-              <h2 className='text-center mb-4'>Sign Up</h2>
-              <Form>
-                <Form.Group id='email'>
-                  <Form.Label>Email</Form.Label>
-                  <Form.Control type='email' ref={emailRef} required></Form.Control>
-                </Form.Group>
-                <Form.Group id='password'>
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control type='password' ref={passwordRef} required></Form.Control>
-                </Form.Group>
-                <Form.Group id='password-confirm'>
-                  <Form.Label>Password Confirmation</Form.Label>
-                  <Form.Control type='password' ref={passwordConfirmRef} required></Form.Control>
-                </Form.Group>
-                <Button type='submit' className='w-100'>Sign Up</Button>
-              </Form>
-            </Card.Body>
-          </Card>
-          <div className='w-100 text-center mt-2'>
-            Already have an account? Log In
-          </div>
-        </div>
-      </Container>
-    </div>
-    
-  );
-};
-
-export default Login;
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Sign Up</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" ref={emailRef} required />
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" ref={passwordRef} required />
+            </Form.Group>
+            <Form.Group id="password-confirm">
+              <Form.Label>Password Confirmation</Form.Label>
+              <Form.Control type="password" ref={passwordConfirmRef} required />
+            </Form.Group>
+            <Button disabled={loading} className="w-100" type="submit">
+              Sign Up
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        Already have an account? <Link to="/login">Log In</Link>
+      </div>
+    </>
+  )
+}
