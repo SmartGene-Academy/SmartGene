cde7f9d363bb327d7aecd22a708a280c8e3f191a
Saai151 <94809929+Saai151@users.noreply.github.com>
2023-08-27 15:43:20 -0400

    Added the sign up page with styling, back end not done

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,27 +1,46 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from '../Components/navbar';
+import { Container, Form, Button, Card } from 'react-bootstrap';
 
-const SignUp = () => {
+const Login = () => {
+  const emailRef = useRef();
+  const passwordRef = useRef(); // Create a useRef reference for the password input
+  const passwordConfirmRef = useRef(); // Create a useRef reference for the password confirmation input
+
   return (
     <div>
-    <NavBar></NavBar>
-    <div className="container">
-      <h1 className="mt-5">About Us</h1>
-      <p className="lead">We are a passionate team dedicated to help students hone their academic skills</p>
-      <div className="row">
-        <div className="col-md-6">
-          <h2>Our Mission</h2>
-          <p>To help students all over Canada get into their program of choice</p>
+      <NavBar></NavBar>
+      <Container className='d-flex allign-items-center justify-content-center' style={{minHeight: "100vh"}}>
+      <div className='w-100' style={{maxWidth: "400px"}}>
+          <Card>
+            <Card.Body>
+              <h2 className='text-center mb-4'>Sign Up</h2>
+              <Form>
+                <Form.Group id='email'>
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type='email' ref={emailRef} required></Form.Control>
+                </Form.Group>
+                <Form.Group id='password'>
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type='password' ref={passwordRef} required></Form.Control>
+                </Form.Group>
+                <Form.Group id='password-confirm'>
+                  <Form.Label>Password Confirmation</Form.Label>
+                  <Form.Control type='password' ref={passwordConfirmRef} required></Form.Control>
+                </Form.Group>
+                <Button type='submit' className='w-100'>Sign Up</Button>
+              </Form>
+            </Card.Body>
+          </Card>
+          <div className='w-100 text-center mt-2'>
+            Already have an account? Log In
+          </div>
         </div>
-        <div className="col-md-6">
-          <h2>Our Values</h2>
-          <p>Lorem ipsum dolor sit amet...</p>
-        </div>
-      </div>
+      </Container>
     </div>
-    </div>
+    
   );
 };
 
-export default SignUp;
+export default Login;
